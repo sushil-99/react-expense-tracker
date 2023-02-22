14ffb7e92db06103e9cac48cf7fb4c8f11b3fdba
sushil badal <sushilbadal123@gmail.com>
2023-02-22 20:56:07 +1100

    updated

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,28 +1,46 @@
-import React from 'react'
-import { Button, Col, Container, Dropdown, Form, FormControl, InputGroup, Row } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Button, Col, Container, Form, FormControl, InputGroup, Row } from 'react-bootstrap'
 
 export const Input = () => {
+    const [formData, setFormData] = useState("")
+
+
+    const handleInput =(e) =>{
+        // console.log(e.target.value)
+      const {name, value} =e.target
+      setFormData({
+        ...formData, [name]:value
+      })
+    }
+    console.log(formData)
+    
+    
+
+    const handleSubmit =() =>{
+        
+    }
+
     return (
         <div>
             <Container>
                 <Row className='mt-5 mb-5'>
                     <Col xs={5} >
                         <InputGroup className='mb-3'>
-                            <FormControl id="input" placeholder="Enter your transactions"
+                            <FormControl name="input" onChange={handleInput} id="input" placeholder="Enter your transactions"
                                 aria-label="transactions" />
                         </InputGroup>
                     </Col>
                     <Col xs={3}>
-                        <FormControl id="amount" type="number" placeholder="Amount.."
+                        <FormControl name='amount' onChange={handleInput} id="amount" type="number" placeholder="Amount"
                             aria-label="transactions" />
                     </Col>
                     <Col>
-                        <FormControl id="date" type="date" placeholder="Amount.."
+                        <FormControl name='date' onChange={handleInput} id="date" type="date"
                             aria-label="transactions" />
                     </Col>
                     <Col>
-                        <Form.Select>
-                            <option>Category</option>
+                        <Form.Select name='category' onChange={handleInput} >
+                            <option value="">Category</option>
                             <option value="income">Income</option>
                             <option value="expenses">Expenses</option>
                         </Form.Select>
